f76ce0fc2acbfaef401b3dc21201a18450eb4562
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-07-19 13:39:29 -0300

    mix new tower_slack

---
+++ lib/tower_slack.ex
@@ -0,0 +1,18 @@
+defmodule TowerSlack do
+  @moduledoc """
+  Documentation for `TowerSlack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TowerSlack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tower_slack_test.exs
@@ -0,0 +1,8 @@
+defmodule TowerSlackTest do
+  use ExUnit.Case
+  doctest TowerSlack
+
+  test "greets the world" do
+    assert TowerSlack.hello() == :world
+  end
+end
