0b34ef23c53e179fab4b6f54e9901767c0d54b68
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-08-01 14:36:38 -0300

    build(deps): update tower

--- lib/tower/slack/reporter.ex
+++ lib/tower/slack/reporter.ex
@@ -2,30 +2,27 @@ defmodule Tower.Slack.Reporter do
   @behaviour Tower.Reporter
 
   @impl true
-  def report_exception(exception, stacktrace, _metadata \\ %{})
-      when is_exception(exception) and is_list(stacktrace) do
+  def report_event(%Tower.Event{kind: :error, reason: exception, stacktrace: stacktrace}) do
     post_message(inspect(exception.__struct__), Exception.message(exception), stacktrace)
   end
 
-  @impl true
-  def report_throw(reason, stacktrace, _metadata \\ %{}) do
+  def report_event(%Tower.Event{kind: :throw, reason: reason, stacktrace: stacktrace}) do
     post_message("Uncaught throw", reason, stacktrace)
   end
 
-  @impl true
-  def report_exit(reason, stacktrace, _metadata \\ %{}) do
+  def report_event(%Tower.Event{kind: :exit, reason: reason, stacktrace: stacktrace}) do
     post_message("Exit", reason, stacktrace)
   end
 
-  @impl true
-  def report_message(level, message, metadata \\ %{})
+  def report_event(%Tower.Event{kind: :message, level: level, reason: message}) do
+    m =
+      if is_binary(message) do
+        message
+      else
+        inspect(message)
+      end
 
-  def report_message(level, message, _metadata) when is_binary(message) do
-    post_message("[#{level}] #{message}", "")
-  end
-
-  def report_message(level, message, _metadata) when is_list(message) or is_map(message) do
-    post_message("[#{level}] #{inspect(message)}", "")
+    post_message("[#{level}] #{m}", "")
   end
 
   defp post_message(kind, reason, stacktrace \\ nil) do
