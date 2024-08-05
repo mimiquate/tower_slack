5d2f0dfa328d4ef62b445aef50b90a5902265ee6
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-08-05 11:36:47 -0300

    feat: includes event id in message (#4)

--- lib/tower/slack/reporter.ex
+++ lib/tower/slack/reporter.ex
@@ -2,19 +2,19 @@ defmodule Tower.Slack.Reporter do
   @behaviour Tower.Reporter
 
   @impl true
-  def report_event(%Tower.Event{kind: :error, reason: exception, stacktrace: stacktrace}) do
-    post_message(inspect(exception.__struct__), Exception.message(exception), stacktrace)
+  def report_event(%Tower.Event{kind: :error, id: id, reason: exception, stacktrace: stacktrace}) do
+    post_message(id, inspect(exception.__struct__), Exception.message(exception), stacktrace)
   end
 
-  def report_event(%Tower.Event{kind: :throw, reason: reason, stacktrace: stacktrace}) do
-    post_message("Uncaught throw", reason, stacktrace)
+  def report_event(%Tower.Event{kind: :throw, id: id, reason: reason, stacktrace: stacktrace}) do
+    post_message(id, "Uncaught throw", reason, stacktrace)
   end
 
-  def report_event(%Tower.Event{kind: :exit, reason: reason, stacktrace: stacktrace}) do
-    post_message("Exit", reason, stacktrace)
+  def report_event(%Tower.Event{kind: :exit, id: id, reason: reason, stacktrace: stacktrace}) do
+    post_message(id, "Exit", reason, stacktrace)
   end
 
-  def report_event(%Tower.Event{kind: :message, level: level, reason: message}) do
+  def report_event(%Tower.Event{kind: :message, id: id, level: level, reason: message}) do
     m =
       if is_binary(message) do
         message
@@ -22,12 +22,12 @@ defmodule Tower.Slack.Reporter do
         inspect(message)
       end
 
-    post_message("[#{level}] #{m}", "")
+    post_message(id, "[#{level}] #{m}", "")
   end
 
-  defp post_message(kind, reason, stacktrace \\ []) do
+  defp post_message(id, kind, reason, stacktrace \\ []) do
     {:ok, _} =
-      Tower.Slack.Message.new(kind, reason, stacktrace)
+      Tower.Slack.Message.new(id, kind, reason, stacktrace)
       |> Tower.Slack.Client.deliver()
 
     :ok
